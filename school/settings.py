875578ca8019bd117ffa3ba7d091db85b368dad9
MugenByte <hamidreza.mugen@gmail.com>
2025-01-16 23:45:48 +0330

    debug = True

--- school/settings.py
+++ school/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1')
+DEBUG = True
 
 ALLOWED_HOSTS = ['school-1qx3.onrender.com']
 
